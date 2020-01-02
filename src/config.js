61b35ee85b4114355a831e731e4917458b436b0e
Gaurav Vaidya <gaurav@renci.org>
2020-01-02 15:57:41 -0500

    Updated JPhyloRef submission URL to reasoner.phyloref.org.
    This will be our permanent server name, which we can change in DNS 
    settings as needed.

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // URL to submit reasoning requests to.
-  JPHYLOREF_SUBMISSION_URL: 'http://phyloref.ggvaidya.com/reason',
+  JPHYLOREF_SUBMISSION_URL: 'http://reasoner.phyloref.org/reason',
 
   // X-Hub-Signature secret used to communicate with JPhyloRef.
   JPHYLOREF_X_HUB_SIGNATURE_SECRET: 'undefined',
